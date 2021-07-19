b90d594576ca45c07340d7da3c60fa7f22c73148
UlfhednarKutral <martinfrobertsweb@gmail.com>
2021-07-19 01:17:16 -0400

    mejora home

--- src/Home/components/Contenido/ContenidoHome.vue
+++ src/Home/components/Contenido/ContenidoHome.vue
@@ -1,53 +1,51 @@
 <template>
-<div class="w-ful h-full mx-auto z-30 relative">
-    <div class="flex flex-row justify-center w-48 mx-auto bg-blue-700 ">
-            <div class="text-center text-white">
-                ENVASES INTELIGENTES
-                <br>
-                PARA EL HOGAR
+    <div class="w-ful h-full mx-auto z-30 relative">
+        <div class="flex flex-row justify-center w-48 mx-auto bg-blue-700 ">
+                <div class="text-center text-white">
+                    ENVASES INTELIGENTES
+                    <br>
+                    PARA EL HOGAR
+                </div>
+        </div>
+        <div class="flex flex-row justify-center mx-auto">
+            <div class="flex flex-col justify-center">
+                    <TicketHome
+                        class="h-14"
+                    />
+                    <TicketHome
+                        class="h-14"
+                    />
+                    <TicketHome
+                        class="h-14"
+                    />
+                    <TicketHome
+                        class="h-14"
+                    />
             </div>
-    </div>
-    <div class="flex flex-row justify-center">
-        <TicketHome
-            class=" h-14"
-        />
-        <div>
-            Alimentos frescos y protegidos
-        <br>
-            durante mucho más tiempo
+            <div class="flex flex-col">
+                <div>
+                    Alimentos frescos y protegidos
+                    <br>
+                    durante mucho más tiempo
+                </div>
+                <div>
+                    Ahorra y reutiliza, evitando el
+                    <br>
+                    desperdicio de alimentos
+                </div>
+                <div>
+                    Organiza tu próximo abastecimiento
+                <br>
+                    de fruta y verdura en 20 días
+                </div>
+                <div >
+                    Optimiza tu espacio dentro del
+                <br>
+                    refrigerador
+                </div>
+            </div>
         </div>
     </div>
-    <div class="flex flex-row  justify-center">
-        <TicketHome
-            class=" h-14"
-        />
-        <div >
-            Ahorra y reutiliza, evitando el
-        <br>
-            desperdicio de alimentos
-        </div>
-    </div>
-    <div class="flex flex-row justify-center">
-        <TicketHome
-            class=" h-14"
-        />
-        <div >
-            Organiza tu próximo avastecimiento
-        <br>
-            de fruta y verdura en 20 días
-        </div>
-    </div>
-    <div class="flex flex-row  justify-center">
-        <TicketHome
-            class=" h-14"
-        />
-        <div >
-            Optimiza tu espacio dentro del
-        <br>
-            refrigerador
-        </div>
-    </div>
-</div>
 </template>
 
 <script>
